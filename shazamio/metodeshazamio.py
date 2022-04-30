98b0fcb74bba399d1583025234ea4d4fdfe92724
alvarocanelo <alvaro.canelo@gmail.com>
2022-04-30 10:05:47 +0200

    Update metodeshazamio.py
    [NEW] Example of get fields from result

--- shazamio/metodeshazamio.py
+++ shazamio/metodeshazamio.py
@@ -5,7 +5,8 @@ from shazamio import Shazam
 async def main():
     shazam = Shazam()
     out = await shazam.recognize_song('C:/Users/Roger/OneDrive/UNi/TFG/PlanA/Codis/shazamio/a.mp3')
-    print(out)
+    result = out['matches'][0]['id]
+    print(result)
 
 loop = asyncio.get_event_loop()
 loop.run_until_complete(main())
