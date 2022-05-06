1924eb04c1df9fe36c23fae20820bd8429a22263
rogervives <103903922+rogervives@users.noreply.github.com>
2022-05-06 13:33:11 +0200

    Update extreure_acords.py
    M'he quedat a mig camí. Seguiré més tard.

--- shazamio/MAIN/Codi/laCuerda/extreure_acords.py
+++ shazamio/MAIN/Codi/laCuerda/extreure_acords.py
@@ -1,11 +1,24 @@
 from bs4 import BeautifulSoup
+import csv
+import requests
 from normalitzar_lacuerda import normalitzar_lacuerda
 
 song = 'Hasta la raíz'
 artist = 'Natalia Lafourcade'
 url = normalitzar_lacuerda(song, artist)
-url_soup = url[8:]+'.html'
 
-soup = BeautifulSoup(open(url_soup), features="lmxl")
+urlpage = requests.get(url)
+soup = BeautifulSoup(urlpage.text, 'html.parser')
 
-print(soup.prettify())
+#print(urlpage.text)
+#print(url)
+
+#haig d'agafar tots els href, és on estan els links per les opcions d'acords.
+opcions = soup.find_all('a')
+llista_opcions = []
+for o in opcions:
+    if 'Letra y acordes' in o:
+        llista_opcions.append(o)
+    print(llista_opcions)
+#print(llista_opcions)
+#print(len(llista_opcions))
