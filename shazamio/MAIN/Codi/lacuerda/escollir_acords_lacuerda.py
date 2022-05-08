9822e937fa1a491a92e2fc685710e186fd45cad5
rogervives <103903922+rogervives@users.noreply.github.com>
2022-05-08 14:42:44 +0200

    Arxiu per escollir acords a laCuerda.net acabat
    Em faltaria pensar algunes possibles prestacions més per a la previsualització dels acords de cada versió.
    Li he canviat el nom una altre vegada.

---
+++ shazamio/MAIN/Codi/lacuerda/escollir_acords_lacuerda.py
@@ -0,0 +1,51 @@
+from bs4 import BeautifulSoup
+import csv
+import webbrowser
+import requests
+from normalitzar_lacuerda import normalitzar_lacuerda
+
+song = 'Hasta la raíz'
+artist = 'Natalia Lafourcade'
+url,a,s = normalitzar_lacuerda(song, artist)
+
+#   Preparar informació per poder treballar amb ella.
+urlpage = requests.get(url)
+soup = BeautifulSoup(urlpage.text, 'html.parser')
+#print(urlpage.text)
+#print(url)
+
+#   Obtenir els links de les opcions disponibles d'acords.
+opcions = soup.find_all('a')
+l_shtml = []
+for o in opcions:
+    #print(o)
+    link = o.get('href')
+    if type(link) == str:
+        if 'shtml' in link:
+            link = link[len(s):]
+            l_shtml.append(link)
+#print(l_shtml)
+
+l_opcions = []
+for i in l_shtml:
+    l_opcions.append(url+i)
+#print(l_opcions)
+#print(llista_opcions)
+#print(len(llista_opcions))
+
+#   Extreure tots els acords que s'utilitzen per poder escollir, és només una previsualització.
+#acords = soup.find_all('pre')
+#for a in acords:
+    #print(a)
+#print(acords[0])
+#print(type(opcions))
+
+#   Input per escollir la opció.
+#   Es podria posar que et dones directament els acords si només hi ha una opció????????????
+print('Opcions disponibles')
+contador = 0
+for i in l_opcions:
+    contador += 1
+    print(contador)
+opcio = input('Escriu quina opció vols: ')
+webbrowser.open(l_opcions[int(opcio)-1])
